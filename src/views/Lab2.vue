71a17da4cedd1525d47b9767e012ece36a957b99
Vadim <vadim.prog.2000@gmail.com>
2020-12-10 23:28:26 +0300

    form complete

--- src/views/Lab2.vue
+++ src/views/Lab2.vue
@@ -16,7 +16,7 @@
 				<label for="input1-x3">x3</label>
 			</div>
 			<div class="input-field col s3">
-				<input type="text" class="validate">
+				<input v-model="cons1" type="text" class="validate">
 			</div>
 		</div>
 		<div class="row">
@@ -33,7 +33,7 @@
 				<label for="input2-x3">x3</label>
 			</div>
 			<div class="input-field col s3">
-				<input type="text" class="validate">
+				<input v-model="cons2" type="text" class="validate">
 			</div>
 		</div>
 		<div class="row">
@@ -50,13 +50,57 @@
 				<label for="input3-x3">x3</label>
 			</div>
 			<div class="input-field col s3">
-				<input type="text" class="validate">
+				<input v-model="cons3" type="text" class="validate">
 			</div>
 		</div>
 		<div class="row">
 			<div class="input-field col s3">
-				<input id="accuracy" type="text" class="validate">
+				<input v-model="accuracy" id="accuracy" type="text" class="validate">
 				<label for="accuracy">Точность</label>
+			</div>
+			<div class="input-field col offset-s6 s3">
+				<button class="btn">Определить</button>
+			</div>
+		</div>
+		<div class="row">
+			<h3>Итог:</h3>
+			<div class="input-field col s6">
+				<h4>Метод Якоби</h4>
+			</div>
+			<div class="input-field col s6">
+				<h4>Метод Зейделя</h4>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="iter1" type="text" class="validate">
+				<label for="iter1" class="active">Количество итераций</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="iter2" type="text" class="validate">
+				<label for="iter2" class="active">Количество итераций</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x1-1" type="text" class="validate">
+				<label for="x1-1" class="active">X1</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x1-2" type="text" class="validate">
+				<label for="x1-2" class="active">X1</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x2-1" type="text" class="validate">
+				<label for="x2-1" class="active">X2</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x2-2" type="text" class="validate">
+				<label for="x2-2" class="active">X2</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x3-1" type="text" class="validate">
+				<label for="x3-1" class="active">X3</label>
+			</div>
+			<div class="input-field col s6">
+				<input disabled id="x3-2" type="text" class="validate">
+				<label for="x3-2" class="active">X3</label>
 			</div>
 		</div>
 	</div>
@@ -66,7 +110,7 @@
 export default {
 	data() {
 		return {
-			input1_x1: '',
+			input1_x1: '123123',
 			input1_x2: '',
 			input1_x3: '',
 			input2_x1: '',
@@ -75,6 +119,10 @@ export default {
 			input3_x1: '',
 			input3_x2: '',
 			input3_x3: '',
+			cons1: '',
+			cons2: '',
+			cons3: '',
+			accuracy: ''
 		}
 	},
 	mounted() {
@@ -126,61 +174,67 @@ export default {
 		return false;
 	},
 
-	// search_click(sender, e) {
-	// 	const arr = [];
-	// 	const cons = [];
-	// 	const iter = [];
-	// 	const Tempiter = [];
-	// 	let iteration = 0;
-	// 	arr[0, 0] = double.Parse(X11.Text);
-	// 	arr[0, 1] = double.Parse(X12.Text);
-	// 	arr[0, 2] = double.Parse(X13.Text);
-	// 	arr[1, 0] = double.Parse(X21.Text);
-	// 	arr[1, 1] = double.Parse(X22.Text);
-	// 	arr[1, 2] = double.Parse(X23.Text);
-	// 	arr[2, 0] = double.Parse(X31.Text);
-	// 	arr[2, 1] = double.Parse(X32.Text);
-	// 	arr[2, 2] = double.Parse(X33.Text);
-	// 	cons[0] = double.Parse(res1.Text);
-	// 	cons[1] = double.Parse(res2.Text);
-	// 	cons[2] = double.Parse(res3.Text);
-	// 	for (int i = 0; i < 3; i++)
-	// 		Tempiter[i] = 0;
-	// 	double accuracy = double.Parse(accuracyBox.Text);
+	search_click(sender, e) {
+		const arr = [];
+		const cons = [];
+		const iter = [];
+		const Tempiter = [];
+		let iteration = 0;
+		arr[0, 0] = this.input1_x1;
+		arr[0, 1] = this.input1_x2;
+		arr[0, 2] = this.input1_x3;
+		arr[1, 0] = this.input2_x1;
+		arr[1, 1] = this.input2_x2;
+		arr[1, 2] = this.input2_x3;
+		arr[2, 0] = this.input3_x1;
+		arr[2, 1] = this.input3_x2;
+		arr[2, 2] = this.input3_x3;
+		cons[0] = this.cons1;
+		cons[1] = this.cons2;
+		cons[2] = this.cons3;
+		for (let i = 0; i < 3; i++) {
+			Tempiter[i] = 0;
+		}
+			
+		let accuracy = this.accuracy;
 
-	// 	diagonal(arr, cons);
+		this.diagonal(arr, cons);
 		
-	// 	do
-	// 	{
-	// 		for (int i = 0; i < 3; i++)
-	// 			iter[i] = Tempiter[i];
-	// 		Tempiter[0] = -arr[0, 1] * iter[1] + (-arr[0, 2]) * iter[2] + cons[0];
-	// 		Tempiter[1] = -arr[1, 0] * iter[0] + (-arr[1, 2]) * iter[2] + cons[1];
-	// 		Tempiter[2] = -arr[2, 0] * iter[0] + (-arr[2, 1]) * iter[1] + cons[2];
-	// 		iteration++;
-	// 	} while (check(Tempiter, iter, accuracy));
-	// 	JacobiX1.Text = Math.Round(Tempiter[0], 3).ToString();
-	// 	JacobiX2.Text = Math.Round(Tempiter[1], 3).ToString();
-	// 	JacobiX3.Text = Math.Round(Tempiter[2], 3).ToString();
-	// 	iterationBox1.Text = iteration.ToString();
-	// 	///// ЗЕЙДЕЕЕЕЛЯЯЯЯ
-	// 	for (int i = 0; i < 3; i++)
-	// 		Tempiter[i] = 0;
-	// 	iteration = 0;
-	// 	do
-	// 	{
-	// 		for (int i = 0; i < 3; i++)
-	// 			iter[i] = Tempiter[i];
-	// 		Tempiter[0] = -arr[0, 1] * Tempiter[1] + (-arr[0, 2]) * Tempiter[2] + cons[0];
-	// 		Tempiter[1] = -arr[1, 0] * Tempiter[0] + (-arr[1, 2]) * Tempiter[2] + cons[1];
-	// 		Tempiter[2] = -arr[2, 0] * Tempiter[0] + (-arr[2, 1]) * Tempiter[1] + cons[2];
-	// 		iteration++;
-	// 	} while (check(Tempiter, iter, accuracy)) ;
-	// 	SeidelX1.Text = Math.Round(Tempiter[0], 3).ToString();
-	// 	SeidelX2.Text = Math.Round(Tempiter[1], 3).ToString();
-	// 	SeidelX3.Text = Math.Round(Tempiter[2], 3).ToString();
-	// 	iterationBox2.Text = iteration.ToString();
-	// }
+		do {
+			for (let i = 0; i < 3; i++) {
+				iter[i] = Tempiter[i];
+			}
+			Tempiter[0] = -arr[0, 1] * iter[1] + (-arr[0, 2]) * iter[2] + cons[0];
+			Tempiter[1] = -arr[1, 0] * iter[0] + (-arr[1, 2]) * iter[2] + cons[1];
+			Tempiter[2] = -arr[2, 0] * iter[0] + (-arr[2, 1]) * iter[1] + cons[2];
+			iteration++;
+		} while (this.check(Tempiter, iter, accuracy));
+
+		JacobiX1.Text = Math.round(Tempiter[0], 3).ToString();
+		JacobiX2.Text = Math.round(Tempiter[1], 3).ToString();
+		JacobiX3.Text = Math.round(Tempiter[2], 3).ToString();
+		iterationBox1.Text = iteration.ToString();
+
+
+		for (let i = 0; i < 3; i++){
+			Tempiter[i] = 0;
+		}
+		iteration = 0;
+		do {
+			for (let i = 0; i < 3; i++) {
+				iter[i] = Tempiter[i];
+			}
+			Tempiter[0] = -arr[0, 1] * Tempiter[1] + (-arr[0, 2]) * Tempiter[2] + cons[0];
+			Tempiter[1] = -arr[1, 0] * Tempiter[0] + (-arr[1, 2]) * Tempiter[2] + cons[1];
+			Tempiter[2] = -arr[2, 0] * Tempiter[0] + (-arr[2, 1]) * Tempiter[1] + cons[2];
+			iteration++;
+		} while (this.check(Tempiter, iter, accuracy)) ;
+
+		SeidelX1.Text = Math.Round(Tempiter[0], 3).ToString();
+		SeidelX2.Text = Math.Round(Tempiter[1], 3).ToString();
+		SeidelX3.Text = Math.Round(Tempiter[2], 3).ToString();
+		iterationBox2.Text = iteration.ToString();
+	}
 }
 </script>
 
